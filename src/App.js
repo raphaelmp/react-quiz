e8a2ee729a52eccec0b08cbd59a87be643d39359
Raphaël M-P <rmarquispelletier@gmail.com>
2019-11-01 22:10:07 -0400

    Cleanup weird rendering architecture

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import QuizComponent from './QuizComponent';
+import WelcomeScreen from './WelcomeScreen';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>ReactQuiz</h1>
-        <p>Engin de quiz plutôt versatile</p>
-      </header>
-      <QuizComponent />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      quizLoaded: 0,
+    }
+  }
+  render() {
+    let topText = null;
+
+    if (this.state.quizLoaded) {
+      topText = <p>Revenir au menu</p>
+    }
+
+    return (
+      <div className="App">
+        {topText}
+        <header className="App-header">
+          <h1>ReactQuiz</h1>
+          <p>Engin de quiz plutôt versatile</p>
+        </header>
+        {this.state.quizLoaded ? <QuizComponent /> : <WelcomeScreen />}
+      </div>
+    )
+  }
+
 }
 
 export default App;
---
+++ src/WelcomeScreen.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+import QuizLoader from './QuizLoader';
+
+class WelcomeScreen extends Component {
+  constructor() {
+    super()
+    this.state = {
+      dataSet: "",
+    }
+
+    this.dataLoader = this.dataLoader.bind(this);
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/payspop.json")
+    .then(response => response.json())
+    .then(response => {
+      let list = [];
+
+      response.map(obj => {
+        let pair = {
+          country: obj.countryLabel,
+          city: obj.cityLabel
+        }
+        list.push(pair)
+
+        return true;
+      });
+
+      this.setState({
+        currentQuestion: list[2].country,
+        tousLesPays: list,
+      });
+    })
+  }
+
+  dataLoader(data){
+    this.setState({
+      dataSet: data,
+    })
+    console.log(this.state)
+  }
+
+  render() {
+    if (this.state.dataSet) {
+      return (
+        <div className="questionContainer">
+          <div className="questionBox">
+            <p className="questionText">Quelle est la ville la plus peuplée de </p>
+            <p>{this.state.currentQuestion ? this.state.currentQuestion : "Chargement en cours..."}</p>
+          </div>
+          <div className="answerBox">
+          </div>
+        </div>
+      )
+    } else  {
+      return (
+        <QuizLoader dataLoader={this.dataLoader}/>
+      )
+    }
+
+  }
+}
+
+export default WelcomeScreen;
